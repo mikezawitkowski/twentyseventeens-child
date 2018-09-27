10c7b24c88255b4fc3471a80498fa6415bec7607
mikezawitkowski <mike@mikezawitkowski.com>
2018-09-27 15:34:25 -0700

    Comment out fonts; change title color

--- functions.php
+++ functions.php
@@ -1,15 +1,14 @@
 <?php
 // 2018-09-27 added per https://www.designbombs.com/add-google-fonts-wordpress/
-function custom_add_google_fonts() {
- wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i', false );
- }
- add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
+// function custom_add_google_fonts() {
+ // wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i', false );
+ // }
+ // add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
 
  function my_theme_enqueue_styles() {
-
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
-
+    $parent_style = 'parent-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
