ef5f726dc5ce02b5ee3894535c7681fb25464a52
mikezawitkowski <mike@mikezawitkowski.com>
2018-09-27 17:12:28 -0700

    Add Google fonts Roboto and Mate

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 // 2018-09-27 added per https://www.designbombs.com/add-google-fonts-wordpress/
-// function custom_add_google_fonts() {
- // wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i', false );
- // }
- // add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
+function custom_add_google_fonts() {
+ wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Mate|Roboto', false );
+ }
+ add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
 
  function my_theme_enqueue_styles() {
     $parent_style = 'parent-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
